db8e0579ff284e4377644bba0f204001ce08569b
Terpal47 <alexander.terp@gmail.com>
2017-07-09 01:12:39 +1000

    Added a lift
    When player steps on lift, elevates to the top of the castle walls. When
    player steps off, lowers back down to the ground.

---
+++ Castle Defender/Assets/_Scripts/LiftController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftController : MonoBehaviour {
+
+    public float speed;
+    public float maxHeight;
+
+    internal bool lift;
+
+    private float minHeight = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        lift = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (lift)
+        {
+            elevate();
+        }
+        else
+        {
+            lower();
+        }
+	}
+
+    public void elevate()
+    {
+        transform.position = new Vector3(transform.position.x, 
+            Mathf.Clamp(transform.position.y + speed * Time.deltaTime, minHeight, maxHeight), 
+            transform.position.z);
+    }
+    
+    public void lower()
+    {
+        transform.position = new Vector3(transform.position.x,
+            Mathf.Clamp(transform.position.y - speed * Time.deltaTime, minHeight, maxHeight),
+            transform.position.z);
+    }
+}
---
+++ Castle Defender/Assets/_Scripts/LiftSensorController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftSensorController : MonoBehaviour {
+
+    private LiftController parentLift;
+
+    void Start()
+    {
+        parentLift = transform.parent.gameObject.GetComponent<LiftController>();
+    }
+
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Body"))
+        {
+            Debug.Log("Player entered");
+            parentLift.lift = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Body"))
+        {
+            Debug.Log("Player exitted");
+            parentLift.lift = false;
+        }
+    }
+}
