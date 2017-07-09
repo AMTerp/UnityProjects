b7b32794adf4a406c6ccfd0c3b8d9d778d862f59
Terpal47 <alexander.terp@gmail.com>
2017-07-10 01:10:08 +1000

    Implemented pausing
    Press escape to pause. When paused, shows controls on the left side of
    the screen.

---
+++ Castle Defender/Assets/_Scripts/PauseController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour {
+
+    private bool paused;
+
+	// Use this for initialization
+	void Start () {
+        paused = false;
+        unpause();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!paused)
+            {
+                pause();
+            }
+            else
+            {
+                unpause();
+            }
+        }
+	}
+
+    void pause()
+    {
+        Time.timeScale = 0;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(true);
+        }
+        paused = true;
+    }
+
+    void unpause()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+        Time.timeScale = 1;
+        paused = false;
+    }
+}
