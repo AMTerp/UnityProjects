e05970b491430c0690e7d85e258c2800337ab5f5
Terpal47 <alexander.terp@gmail.com>
2017-07-11 00:05:42 +1000

    Added functional buttons to purchase a machine gun
    And one to buy a pistol. However, player starts with pistol.

---
+++ Castle Defender/Assets/_Scripts/BuyWeaponButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyWeaponButton : MonoBehaviour {
+
+    public float cost;
+    public int weaponSlot;
+    public GameObject weapon;
+
+    private Button button;
+
+    // Use this for initialization
+    void Start () {
+        button = gameObject.GetComponent<Button>();
+
+        button.onClick.AddListener(buyWeapon);
+    }
+	
+	void buyWeapon()
+    {
+        // Check that player does not already have the weapon.
+        Transform mainCamera = GameObject.FindWithTag("Player").transform.Find("Main Camera");
+        Transform weaponSlotObject;
+        for (int i = 0; i < mainCamera.childCount; i++)
+        {
+            weaponSlotObject = mainCamera.GetChild(i);
+            if (weaponSlotObject.childCount > 0 && weaponSlotObject.GetChild(0).gameObject.name.Equals(weapon.name))
+            {
+                // Already have weapon. Return.
+                return;
+            }
+        }
+
+        // Got this far, must not already have weapon.
+        GameObject newWeapon = Instantiate(weapon, mainCamera.Find("Weapon Slot " + weaponSlot).transform);
+        newWeapon.transform.GetChild(0).gameObject.SetActive(false);
+        newWeapon.name = weapon.name;
+        //newWeapon.transform.SetParent(mainCamera.Find("Weapon Slot " + weaponSlot));
+    }
+}
